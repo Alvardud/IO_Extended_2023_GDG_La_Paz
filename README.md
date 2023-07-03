ca57e4135710f8bc4f2802766dd77fd13fb00dfc
Alvaro Martinez <alvaro_dudutex@outlook.es>
2023-07-03 09:39:37 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# IO Extended GDG La Paz
+
+Aplicación para evento Google IO Extended 2023.
---
+++ android/app/src/main/kotlin/com/example/io_extended_2023_gdg_la_paz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.io_extended_2023_gdg_la_paz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
